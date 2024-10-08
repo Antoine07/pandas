01d7f112e15f2b1e00e4a7ed7c34ed3cfc46e9b8
Antoine <antoine.lucsko@wanadoo.fr>
2024-10-08 10:13:01 +0200

    Update exercice_synthese.py

--- 02_Numpy/exercice_synthese.py
+++ 02_Numpy/exercice_synthese.py
@@ -4,3 +4,14 @@ import math as m
 january = np.array([-2,  5, -5,  6, -2,  0,  6,  2,  8,  0,  6, -1,  3,  3,  7,  0, -5, 7,  4,  7,  8, -1,  5, -2,  3, -3, -2,  7,  8,  4,  2])
 
 
+
+# Affiche toute les température positive et que l'on en fasse la somme
+
+# Comparaison est-ce qu'il y a plus de température positive que négative
+
+
+# Moyenne des températures sur le mois de Janvier
+
+
+# Remplacez les températures < 0 par la température moyenne
+
