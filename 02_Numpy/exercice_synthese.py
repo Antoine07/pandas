0f4529edc1b42cf13a9e5417e2274ceb945faf58
Antoine <antoine.lucsko@wanadoo.fr>
2024-10-08 10:11:21 +0200

    Create exercice_synthese.py

---
+++ 02_Numpy/exercice_synthese.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import math as m
+
+january = np.array([-2,  5, -5,  6, -2,  0,  6,  2,  8,  0,  6, -1,  3,  3,  7,  0, -5, 7,  4,  7,  8, -1,  5, -2,  3, -3, -2,  7,  8,  4,  2])
+
+
